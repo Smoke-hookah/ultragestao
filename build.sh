1f1554826960e2ae254c6ca145801786b8e70f0b
Smoke-hookah <feitoza19@hotmail.com>
2026-05-12 00:45:58 -0300

    Fix: Removendo --with-deps do build

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 # Instalar dependências do Python
 pip install -r requirements.txt
 
-# Instalar navegadores do Playwright e dependências do sistema
-playwright install --with-deps chromium
+# Instalar navegadores do Playwright
+playwright install chromium
